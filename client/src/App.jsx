3bd347ac1a9a76b6a27d909841e1a67d678905f3
satyamJha002 <satyam073000@gmail.com>
2024-05-03 22:54:14 +0530

    Create a react route for pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 const App = () => {
-  return <div className="text-2xl font-bold">Hello world!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
